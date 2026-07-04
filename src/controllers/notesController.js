ae8e8b30a9b8f0604b5bbe8d0a78e56da451028e
Valentyn <manilchuk.v@gmail.com>
2026-07-04 17:45:15 +0300

    fix notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -45,7 +45,7 @@ export const updateNote = async (req, res) => {
   );
 
   if (!note) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
