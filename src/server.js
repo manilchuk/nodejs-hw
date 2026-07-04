0d17c269fbc4bc70bceb27a2a4cff35891826cb4
Valentyn <manilchuk.v@gmail.com>
2026-07-04 17:01:25 +0300

    feat: complete notes homework

--- src/server.js
+++ src/server.js
@@ -2,70 +2,32 @@
 
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import 'dotenv/config';
 import helmet from 'helmet';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { logger } from './middleware/logger.js';
+import notsRoutes from './routes/notesRoutes.js';
 
 const app = express();
-
-app.use(helmet());
-app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
-app.use(express.json());
-
 const PORT = process.env.PORT ?? 3000;
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({
-    message: 'Retrieved all notes',
-  });
-});
+app.use(helmet());
+app.use(logger);
+app.use(express.json());
+app.use(cors());
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-
-  res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
-  });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
+app.use(notsRoutes);
 
 // Middleware для обробки неіснуючих маршрутів
-app.use((req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
+app.use(notFoundHandler);
 
 // Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  const isProd = process.env.NODE_ENV === 'production';
+app.use(errorHandler);
 
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.stack,
-  });
-});
-console.log('NODE_ENV:', process.env.NODE_ENV);
+// підключення до MongoDB
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
